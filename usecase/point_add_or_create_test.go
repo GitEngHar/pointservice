79ed76431416e15c2f3245b7735aad8214a5aea0
GitEngHar <harukii1999@gmail.com>
2025-06-08 10:57:10 +0900

    add testcase

--- usecase/point_add_or_create_test.go
+++ usecase/point_add_or_create_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"github.com/google/go-cmp/cmp"
 	"pointservice/domain"
 	"testing"
 )
@@ -49,7 +50,82 @@ func (s stubPointRepository) UpdatePointOrCreateByUserID(ctx context.Context, po
 }
 
 func Test_Execute(t *testing.T) {
+	type args struct {
+		context.Context
+		*PointAddOrCreateInput
+	}
+
 	t.Parallel()
+
 	mockRepo := stubPointRepository{}
 	uc := NewPointAddOrCreateInterceptor(mockRepo)
+	tests := []struct {
+		name        string
+		args        args
+		expectedErr bool
+		errMsg      string
+	}{
+		{
+			name: "Successful point add",
+			args: args{
+				context.Background(),
+				&PointAddOrCreateInput{
+					"userA",
+					123,
+				},
+			},
+			expectedErr: false,
+			errMsg:      "",
+		},
+		{
+			name: "Successful point create",
+			args: args{
+				context.Background(),
+				&PointAddOrCreateInput{
+					"123test",
+					123,
+				},
+			},
+			expectedErr: false,
+			errMsg:      "",
+		},
+		{
+			name: "Failed select target user",
+			args: args{
+				context.Background(),
+				&PointAddOrCreateInput{
+					"!",
+					123,
+				},
+			},
+			expectedErr: true,
+			errMsg:      "failed select target user: unpredictable errors for testing",
+		},
+		{
+			name: "Failed generate new point",
+			args: args{
+				context.Background(),
+				&PointAddOrCreateInput{
+					"123test",
+					-1,
+				},
+			},
+			expectedErr: true,
+			errMsg:      "new point create filed: points must be greater than 0",
+		},
+	}
+
+	for _, ts := range tests {
+		t.Run(ts.name, func(t *testing.T) {
+			err := uc.Execute(ts.args.Context, ts.args.PointAddOrCreateInput)
+			var errMsg string
+			if ts.expectedErr {
+				errMsg = err.Error()
+				if diff := cmp.Diff(ts.errMsg, errMsg); diff != "" {
+					t.Error(diff)
+				}
+			}
+		})
+	}
+
 }
