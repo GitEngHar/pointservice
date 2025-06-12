c85920daaf6f5628111095d2a4ae41679a58942b
GitEngHar <harukii1999@gmail.com>
2025-06-12 22:24:07 +0900

    fix column

--- adapter/repository/point_mysql.go
+++ adapter/repository/point_mysql.go
@@ -20,7 +20,7 @@ func NewPointSQL(db *sql.DB) PointRepository {
 }
 
 func (p PointRepository) GetPointByUserID(ctx context.Context, userID string) (domain.Point, error) {
-	var query = `SELECT point_num FROM point_root WHERE user_id=?`
+	var query = `SELECT point_num,created_at,updated_at FROM point_root WHERE user_id=?`
 	var pointNum int
 	var createdAt time.Time
 	var updatedAt time.Time
@@ -41,10 +41,11 @@ func (p PointRepository) GetPointByUserID(ctx context.Context, userID string) (d
 }
 
 func (p PointRepository) UpdatePointByUserID(ctx context.Context, point domain.Point) error {
-	var query = `UPDATE point_root SET point_num=? WHERE user_id=?`
-	_, err := p.db.Exec(query, point.PointNum, point.UserID)
+	var query = `UPDATE point_root SET point_num=?, updated_at=? WHERE user_id=?`
+	_, err := p.db.Exec(query, point.PointNum, point.UpdatedAt, point.UserID)
 	if err != nil {
-		return fmt.Errorf("failed to scan point row: %w (point_num:%d user_id:%s)", err, point.PointNum, point.UserID)
+		return fmt.Errorf("failed to update point: %w (user_id:%s, point_num:%d, created_at:%v, updated_at:%v)",
+			err, point.UserID, point.PointNum, point.CreatedAt, point.UpdatedAt)
 	}
 	return nil
 }
@@ -59,7 +60,9 @@ func (p PointRepository) UpdatePointOrCreateByUserID(ctx context.Context, point 
 				`
 	_, err := p.db.Exec(query, point.UserID, point.PointNum, point.CreatedAt, point.UpdatedAt)
 	if err != nil {
-		return fmt.Errorf("failed to scan point row: %w (point_num:%d user_id:%s)", err, point.PointNum, point.UserID, point.CreatedAt, point.UpdatedAt)
+		return fmt.Errorf("failed to insert or update point: %w (user_id:%s, point_num:%d, created_at:%v, updated_at:%v)",
+			err, point.UserID, point.PointNum, point.CreatedAt, point.UpdatedAt)
+
 	}
 	return nil
 }
--- domain/point_test.go
+++ domain/point_test.go
@@ -3,13 +3,16 @@ package domain
 import (
 	"github.com/google/go-cmp/cmp"
 	"testing"
+	"time"
 )
 
 func Test_NewPoint(t *testing.T) {
 	t.Parallel()
 	type args struct {
-		UserID   string
-		PointNum int
+		UserID    string
+		PointNum  int
+		CreatedAt time.Time
+		UpdatedAt time.Time
 	}
 	tests := []struct {
 		name     string
@@ -20,20 +23,26 @@ func Test_NewPoint(t *testing.T) {
 		{
 			name: "Successful create point struct",
 			args: args{
-				UserID:   "abc123d",
-				PointNum: 1000,
+				UserID:    "abc123d",
+				PointNum:  1000,
+				CreatedAt: time.Date(2025, time.June, 10, 15, 30, 0, 0, time.UTC),
+				UpdatedAt: time.Date(2025, time.June, 10, 15, 30, 0, 0, time.UTC),
 			},
 			expected: Point{
-				UserID:   "abc123d",
-				PointNum: 1000,
+				UserID:    "abc123d",
+				PointNum:  1000,
+				CreatedAt: time.Date(2025, time.June, 10, 15, 30, 0, 0, time.UTC),
+				UpdatedAt: time.Date(2025, time.June, 10, 15, 30, 0, 0, time.UTC),
 			},
 			errMsg: "",
 		},
 		{
 			name: "Failed create point struct",
 			args: args{
-				UserID:   "!abc123d",
-				PointNum: 1000,
+				UserID:    "!abc123d",
+				PointNum:  1000,
+				CreatedAt: time.Date(2025, time.June, 10, 15, 30, 0, 0, time.UTC),
+				UpdatedAt: time.Date(2025, time.June, 10, 15, 30, 0, 0, time.UTC),
 			},
 			expected: Point{},
 			errMsg:   "userID is not correct format: !abc123d",
@@ -41,8 +50,10 @@ func Test_NewPoint(t *testing.T) {
 		{
 			name: "Failed create point struct",
 			args: args{
-				UserID:   "abc123d",
-				PointNum: -1,
+				UserID:    "abc123d",
+				PointNum:  -1,
+				CreatedAt: time.Date(2025, time.June, 10, 15, 30, 0, 0, time.UTC),
+				UpdatedAt: time.Date(2025, time.June, 10, 15, 30, 0, 0, time.UTC),
 			},
 			expected: Point{},
 			errMsg:   "points must be greater than 0",
@@ -51,7 +62,7 @@ func Test_NewPoint(t *testing.T) {
 
 	for _, ts := range tests {
 		t.Run(ts.name, func(t *testing.T) {
-			result, err := NewPoint(ts.args.UserID, ts.args.PointNum)
+			result, err := NewPoint(ts.args.UserID, ts.args.PointNum, ts.args.CreatedAt, ts.args.UpdatedAt)
 			if diff := cmp.Diff(ts.expected, result); diff != "" {
 				t.Error(diff)
 			}
